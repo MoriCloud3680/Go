0474fd5ec72c62a15386ab2fd8dbe9dfe2c9f672
MoriCloud3680 <jyyakg@gmail.com>
2025-05-05 02:20:50 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,6 +32,11 @@ def save_recommended_numbers(round_no, numbers, tag):
     client = authenticate_google()
     sheet = client.open("Go").worksheet("F10")
     today_date = pd.Timestamp.now().strftime("%Y-%m-%d")
+    
+    # JSON 호환성 보장 (표준 int와 문자열만 사용)
+    round_no = int(round_no)  
+    numbers = ",".join(str(int(num)) for num in numbers.split(","))
+    
     sheet.append_row([today_date, round_no, tag, numbers])
 
 # GA 모델 함수 (추천 번호 생성)
