a884d5d19c374348e0c68b016d6e6114a1eef913
MoriCloud3680 <jyyakg@gmail.com>
2025-05-05 09:33:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from utils import update_after_input
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def home():
-    update_after_input()
-    return '추천번호 10개 자동 업데이트 완료!'
+    update_after_input()   # 명시적으로 호출
+    return "GA 모델이 실행되었습니다.", 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)  # Render는 포트 10000 추천
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
