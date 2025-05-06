1718a01b58c3ad83fd6aa2c00d1ae4c3f75c690a
MoriCloud3680 <jyyakg@gmail.com>
2025-05-06 22:48:21 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import random
 from datetime import datetime
 
-# Google 인증
+# Google 인증 함수
 def authenticate_google():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     google_credentials_json = os.getenv('GOOGLE_CREDENTIALS')
@@ -14,7 +14,7 @@ def authenticate_google():
     client = gspread.authorize(creds)
     return client
 
-# 최신 회차 데이터 가져오기
+# 최신 회차 데이터 불러오기
 def fetch_current_round():
     client = authenticate_google()
     sheet_id = "1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo"
@@ -23,52 +23,16 @@ def fetch_current_round():
     actual_numbers = sheet.acell('C2').value
     return round_no, actual_numbers
 
-# 시트에 기록하는 함수
+# Prev Best 전략 (직전 회차에서 10개 랜덤 추출)
+def prev_best(actual_numbers):
+    numbers_pool = [int(n) for n in actual_numbers.split(",")]
+    selected_numbers = sorted(random.sample(numbers_pool, 10))
+    return ",".join([f"{num:02d}" for num in selected_numbers])
+
+# 시트 업데이트 함수
 def update_recommendations(round_no, numbers, tag):
     client = authenticate_google()
     sheet_id = "1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo"
     f10_sheet = client.open_by_key(sheet_id).worksheet("F10")
     today_date = datetime.now().strftime('%Y-%m-%d')
     f10_sheet.insert_row([today_date, round_no, tag, numbers], 2, value_input_option="USER_ENTERED")
-
-# GA 단기 최적화 전략 (직전 1회차만 활용)
-def ga_single_round_optimization(actual_numbers):
-    actual_pool = [int(n) for n in actual_numbers.split(",")]
-
-    def fitness(combo):
-        return len(set(combo) & set(actual_pool))
-
-    def generate_combo():
-        return random.sample(range(1, 71), 10)
-
-    population_size = 100
-    generations = 30
-    mutation_rate = 0.1
-
-    population = [generate_combo() for _ in range(population_size)]
-
-    for _ in range(generations):
-        population.sort(key=fitness, reverse=True)
-        next_generation = population[:20]
-
-        while len(next_generation) < population_size:
-            parents = random.sample(population[:20], 2)
-            crossover_point = random.randint(1, 9)
-            child = parents[0][:crossover_point] + parents[1][crossover_point:]
-
-            if random.random() < mutation_rate:
-                mutation_index = random.randint(0, 9)
-                child[mutation_index] = random.randint(1, 70)
-
-            child = list(set(child))
-            while len(child) < 10:
-                new_num = random.randint(1, 70)
-                if new_num not in child:
-                    child.append(new_num)
-
-            next_generation.append(child)
-
-        population = next_generation
-
-    best_combination = max(population, key=fitness)
-    return ",".join([f"{num:02d}" for num in sorted(best_combination)])
