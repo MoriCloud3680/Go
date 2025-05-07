67b21788d3126becb89fd491f784c7cf20f81139
MoriCloud3680 <jyyakg@gmail.com>
2025-05-07 13:27:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,29 @@
-import os
-import json
-import gspread
 from flask import Flask
-from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
-import random
+from utils import fetch_current_round, ga_recent_best, update_recommendations
 
 app = Flask(__name__)
 
+# 전역변수로 최근 생성한 회차 기록
 last_generated_round = None
-
-# 구글 인증
-def authenticate_google():
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    google_credentials_json = os.getenv('GOOGLE_CREDENTIALS')
-    credentials_dict = json.loads(google_credentials_json)
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
-    return gspread.authorize(creds)
-
-# 최신 회차 데이터 불러오기
-def fetch_current_round():
-    client = authenticate_google()
-    sheet_id = "1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo"
-    sheet = client.open_by_key(sheet_id).worksheet("Actual22")
-    
-    round_no = int(sheet.acell('A2').value)
-    actual_numbers = sheet.acell('B2').value.replace(" ", "")
-    return round_no, actual_numbers
-
-# GA Best 전략 (5개 번호 고정 + 랜덤 5개 추가)
-def ga_best(actual_numbers):
-    recent_nums = [int(n) for n in actual_numbers.split(",")]
-    fixed_nums = random.sample(recent_nums, 5)
-    remaining_pool = [n for n in range(1, 71) if n not in fixed_nums]
-    random_nums = random.sample(remaining_pool, 5)
-    combined_nums = sorted(fixed_nums + random_nums)
-    return ",".join([f"{num:02d}" for num in combined_nums])
-
-# 시트 업데이트 함수 (날짜 제외)
-def update_recommendations(round_no, numbers, tag):
-    client = authenticate_google()
-    sheet_id = "1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo"
-    f10_sheet = client.open_by_key(sheet_id).worksheet("F10")
-    # 날짜(today_date) 제외
-    f10_sheet.insert_row([round_no, tag, numbers], 2, value_input_option="USER_ENTERED")
 
 @app.route("/", methods=["GET"])
 def home():
     global last_generated_round
 
-    current_round, actual_numbers = fetch_current_round()
+    current_round = fetch_current_round()
     next_round = current_round + 1
 
+    # 회차가 중복 생성되지 않도록 체크
     if next_round != last_generated_round:
         try:
-            ga_numbers = ga_best(actual_numbers)
-            update_recommendations(next_round, ga_numbers, "GA Best")
+            recent_best_numbers = ga_recent_best()
+
+            update_recommendations(next_round, recent_best_numbers, "GA Recent5 Best")
 
             last_generated_round = next_round
-            return f"✅ {next_round}회차 GA Best 조합 생성 완료.", 200
+
+            return f"✅ {next_round}회차 GA Recent5 Best 조합 생성 완료.", 200
+
         except Exception as e:
             return f"❌ 오류 발생: {str(e)}", 500
     else:
