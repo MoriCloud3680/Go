a345ce8092874eb3165a2f0c923502f394b7495d
MoriCloud3680 <jyyakg@gmail.com>
2025-05-11 19:28:47 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,64 +1,77 @@
+# Colab에서 Google Sheets 연동 Attention LSTM 모델 세부 튜닝 (Render 환경변수 사용)
+
+# 패키지 설치
+!pip install numpy pandas statsmodels tensorflow keras gspread oauth2client
+
+# 라이브러리 임포트
+import os
+import json
 import numpy as np
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, LSTM, Dense, Bidirectional, Attention
-from utils import authenticate_google
 
-def train_and_update():
-    # Google Sheets 인증 및 데이터 로딩
-    client = authenticate_google()
-    sheet_url = 'https://docs.google.com/spreadsheets/d/1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo/edit#gid=0'
-    sheet = client.open_by_url(sheet_url).worksheet('ActualNumbers')
-    data = pd.DataFrame(sheet.get_all_records())
+# Google Sheets 인증 (Render의 환경변수 사용)
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials_dict = json.loads(os.getenv('GOOGLE_CREDENTIALS'))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
+client = gspread.authorize(creds)
 
-    numbers_series = data['ActualNumbers'].apply(lambda x: list(map(int, x.split(','))))
-    flattened_numbers = np.array([num for sublist in numbers_series for num in sublist])
+# Google Sheets 데이터 로딩
+sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo/edit#gid=0').worksheet('ActualNumbers')
+data = sheet.get_all_records()
+df = pd.DataFrame(data)
 
-    moving_avg = pd.Series(flattened_numbers).rolling(window=5).mean().bfill()
-    diff_numbers = np.diff(flattened_numbers, prepend=flattened_numbers[0])
+# 데이터 전처리 및 특성공학
+numbers_series = df['ActualNumbers'].apply(lambda x: list(map(int, x.split(','))))
+flattened_numbers = np.array([num for sublist in numbers_series for num in sublist])
 
-    features = np.column_stack((flattened_numbers, moving_avg, diff_numbers))
+moving_avg = pd.Series(flattened_numbers).rolling(window=5).mean().bfill()
+diff_numbers = np.diff(flattened_numbers, prepend=flattened_numbers[0])
 
-    # 데이터 스케일링
-    scaler = MinMaxScaler()
-    features_scaled = scaler.fit_transform(features)
+features = np.column_stack((flattened_numbers, moving_avg, diff_numbers))
 
-    window_size = 20
-    X, y = [], []
-    for i in range(len(features_scaled) - window_size):
-        X.append(features_scaled[i:i + window_size])
-        y.append(features_scaled[i + window_size, 0])
+# 데이터 스케일링
+scaler = MinMaxScaler()
+features_scaled = scaler.fit_transform(features)
 
-    X, y = np.array(X), np.array(y)
+# 튜닝된 파라미터
+window_size = 20
 
-    # 모델 정의 및 학습
-    input_layer = Input(shape=(window_size, features_scaled.shape[1]))
-    lstm_out = Bidirectional(LSTM(128, activation='relu', return_sequences=True))(input_layer)
-    attention_out = Attention()([lstm_out, lstm_out])
-    attention_last_step = attention_out[:, -1, :]
-    dense_out = Dense(64, activation='relu')(attention_last_step)
-    output_layer = Dense(1)(dense_out)
+X, y = [], []
+for i in range(len(features_scaled) - window_size):
+    X.append(features_scaled[i:i + window_size])
+    y.append(features_scaled[i + window_size, 0])
 
-    model = Model(inputs=input_layer, outputs=output_layer)
-    model.compile(optimizer='adam', loss='mse')
-    model.fit(X, y, epochs=50, batch_size=32)
+X, y = np.array(X), np.array(y)
 
-    # 모델 저장
-    model.save('model/model.h5')
+# 세부 튜닝된 Attention LSTM 모델
+input_layer = Input(shape=(window_size, features_scaled.shape[1]))
+lstm_out = Bidirectional(LSTM(128, activation='relu', return_sequences=True))(input_layer)
+attention_out = Attention()([lstm_out, lstm_out])
+attention_last_step = attention_out[:, -1, :]
+dense_out = Dense(64, activation='relu')(attention_last_step)
+output_layer = Dense(1)(dense_out)
 
-    # 예측 수행
-    predictions = model.predict(X)
-    predictions_inverse = scaler.inverse_transform(
-        np.column_stack((predictions, np.zeros((len(predictions), 2))))
-    )[:, 0]
+model = Model(inputs=input_layer, outputs=output_layer)
+model.compile(optimizer='adam', loss='mse')
 
-    # Google Sheets에 예측값 업데이트
-    predicted_sheet = client.open_by_url(sheet_url).worksheet('Predicted')
-    latest_round = int(data['Round'].iloc[-1]) + 1
-    numbers_str = ",".join(map(str, predictions_inverse.flatten().round().astype(int)[:10]))
-    predicted_sheet.append_row([latest_round, numbers_str], value_input_option='USER_ENTERED')
+# 튜닝된 학습 조건
+model.fit(X, y, epochs=50, batch_size=32)
 
-    # 상태 업데이트
-    status_sheet = client.open_by_url(sheet_url).worksheet('Status')
-    status_sheet.update('A2', latest_round)
+# 예측 및 Google Sheets에 업데이트
+predictions = model.predict(X)
+predictions_inverse = scaler.inverse_transform(np.column_stack((predictions, np.zeros((len(predictions), 2)))))[:, 0]
+
+predicted_sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo/edit#gid=0').worksheet('Predicted')
+latest_round = int(df['Round'].iloc[-1]) + 1
+numbers_str = ",".join(map(str, predictions_inverse.flatten().round().astype(int)[:10]))
+predicted_sheet.append_row([latest_round, numbers_str], value_input_option='USER_ENTERED')
+
+status_sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1P-kCWRZk0YJFokgQuwVpxg_dKz78xN0PqwBmgtf63fo/edit#gid=0').worksheet('Status')
+status_sheet.update('A2', latest_round)
+
+print("Optimized Attention LSTM Predictions:", predictions_inverse.flatten()[:20])
